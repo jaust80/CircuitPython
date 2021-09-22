f45880cd0227411b68d51e1b471d4e44b3fc27a4
Jai Aust <jaust80@ccs.local>
2021-09-22 10:41:43 -0400

    Sensor code

--- Code/sensor.py
+++ Code/sensor.py
@@ -4,9 +4,10 @@ import adafruit_hcsr04
 import neopixel
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D3, echo_pin=board.D2)
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
-pos = 1;
+pos = 1
 print("new code! 2")
 time.sleep(1)
+dot.brightness = 0.1
 
 while True:
     try:
@@ -16,15 +17,22 @@ while True:
         print("Retrying!")
     time.sleep(0.01)
 
-    if pos < 5 or pos > 20:
+    if pos < 5:
         r = 255
         b = 0
         g = 0
+    elif pos < 20:
+        r = 255-((pos-5)/15*255)
+        g = 0
+        b = (pos-5)/15*255
+    elif pos < 35:
+        r = 0
+        g = (pos-20)/15*255
+        b = 255-((pos-20)/15*255)
     else:
         r = 0
-        b = 255
-        g = 0
-    dot.fill((r,g,b))
+        g = 255
+        b = 0
 
-
+    dot.fill((int(r), int(g), int(b)))
 
